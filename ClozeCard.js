334489ff61a46d7f27e4d3332d2772f2efff6678
Manisha Pawar <manishapawar.net@gmail.com>
2017-09-11 16:59:19 -0500

    Update ClozeCard.js

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,1 +1,17 @@
+function ClozeCard(text, cloze) {
+    this.text = text.split(cloze);
+    this.cloze = cloze;
 
+};
+
+
+function ClozeCardPrototype() {
+
+    this.clozeRemoved = function () {
+        return `${this.text[0]} ... ${this.text[1]}`;  
+    };											
+};
+
+ClozeCard.prototype = new ClozeCardPrototype();
+
+module.exports = ClozeCard; 
